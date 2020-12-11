d7fbfa8624f5c003f990777c106249b05f5d238f
JonNavaridas <56338855+JonNavaridas@users.noreply.github.com>
2020-12-11 17:22:50 +0100

    Primer Comit
    Se ha creado el proyecto en eclipse

---
+++ Aplicaciones/WorkSpace/Aplicacion de Escritorio/src/interfazVisual/Dibina.java
@@ -0,0 +1,13 @@
+package interfazVisual;
+
+import javax.swing.JFrame;
+
+public class Dibina extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		Dibina app = new Dibina();
+		app.setVisible(true);
+	}
+}
